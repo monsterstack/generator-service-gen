7d8496ed6c9e62333abab0a10c7bb7dabfbd4c16
zacharyrote <zachary.rote@gmail.com>
2017-02-22 12:44:45 -0500

    generator-service-gen

--- generators/app/templates/cluster.js
+++ generators/app/templates/cluster.js
@@ -29,6 +29,9 @@ const main = () => {
 
   cluster.onProxyReady((proxy) => {
     console.log("Yeah.. the proxy is bound");
+    setInterval(() => {
+      cluster.reannounce(config);
+    }, 2*60*1000);
   });
 }
 
